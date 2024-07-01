26d779ab1a9498299b6cb24f5be4fe352a183134
Hai Le <haile.vnm@gmail.com>
2024-07-01 18:04:39 +0700

    feat: add icon to the app

--- src/app/receipts/[id]/page.tsx
+++ src/app/receipts/[id]/page.tsx
@@ -1,5 +1,11 @@
 import Button from '@/components/ui/button';
 import { Recipe } from '@/models/recipe';
+import {
+  AccessTimeOutlined,
+  Add,
+  LocalPrintshopOutlined,
+  ScatterPlot
+} from '@mui/icons-material';
 import { Box, Breadcrumbs, Divider, Link, Typography } from '@mui/material';
 import Image from 'next/image';
 
@@ -25,7 +31,7 @@ export default async function ReceiptDetail({ params }: { params: { id: string }
   ];
 
   return <>
-    <div className='grid grid-cols-2 gap-4'>
+    <div className='grid grid-cols-2 gap-6'>
       <Box>
         <Breadcrumbs separator="›" aria-label="breadcrumb">
           {breadcrumbs}
@@ -35,28 +41,42 @@ export default async function ReceiptDetail({ params }: { params: { id: string }
           { recipe.desc }
         </Box>
 
-        <Box display={'flex'} marginY={2} gap={3} fontWeight={500}>
-          {
-            recipe.steps.map((step, index) => (<Box key={index}>
-              <p className='uppercase text-xs'>{step.title}</p>
-              <p>{step.durationMin}</p>
-            </Box>))
-          }
+        <Box display={'flex'} marginY={2} fontWeight={500}>
+          <Box display={'flex'} marginRight={1}>
+            <AccessTimeOutlined fontSize={'large'}></AccessTimeOutlined>
+          </Box>
+          <Box display={'flex'} gap={3}>
+            {
+              recipe.steps.map((step, index) => (<Box key={index}>
+                <p className='uppercase text-xs'>{step.title}</p>
+                <p>{step.durationMin}</p>
+              </Box>))
+            }
+          </Box>
         </Box>
         <Divider></Divider>
         <Box display={'flex'} marginY={1} fontWeight={500} justifyContent={'space-between'}>
-          <Box>
-            <p className='uppercase text-xs'>Yeild</p>
-            <p>
-              {
-                recipe.yield.map(item => [item.amount, item.title].join(' ')).join(', ')
-              }
-            </p>
+          <Box display={'flex'}>
+            <Box display={'flex'} marginRight={1}>
+              <ScatterPlot fontSize={'large'}></ScatterPlot>
+            </Box>
+            <Box>
+              <p className='uppercase text-xs'>Yeild</p>
+              <p>
+                {
+                  recipe.yield.map(item => [item.amount, item.title].join(' ')).join(', ')
+                }
+              </p>
+            </Box>
           </Box>
           <Box display={'flex'} justifyContent={'flex-end'} gap={1}>
             <Box>
-              <Button variant={'outlined'} size={'small'} color={'primary'} className='!mr-2 !mb-2'>Save Recipe</Button>
-              <Button variant={'outlined'} size={'small'} color={'primary'} className='!mb-2'>Print</Button>
+              <Button variant={'outlined'} size={'small'} color={'primary'} className='!mr-2 !mb-2'>
+                <Add></Add> Save Recipe
+              </Button>
+              <Button variant={'outlined'} size={'small'} color={'primary'} className='!mb-2'>
+                <LocalPrintshopOutlined></LocalPrintshopOutlined> Print
+              </Button>
             </Box>
           </Box>
         </Box>
